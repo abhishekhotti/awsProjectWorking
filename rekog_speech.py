e355e00873370492b85f8a4b8d0777348d2a7671
ahotti <ahotti@pulse-100030.psecure.net>
2019-11-21 11:50:37 -0800

    have not tested in rekog_speech yet

--- rekog_speech.py
+++ rekog_speech.py
@@ -1,9 +1,10 @@
 import boto3
 from account import bucketName, workingDir
-
+import requests
+import time
 
 def detect_text(photo, bucket):
-    client = boto3.client("rekognition")
+    client = boto3.client("rekognition", region_name="us-west-2")
     response = client.detect_text(Image={"S3Object": {"Bucket": bucket, "Name": photo}})
     textDetections = response["TextDetections"]
     textInPic = ""
@@ -14,7 +15,7 @@ def detect_text(photo, bucket):
 
 
 def getSpeech(text, voiceActor):
-    polly_client = boto3.client("polly")
+    polly_client = boto3.client("polly", region_name="us-west-2")
     response = polly_client.synthesize_speech(
         VoiceId=voiceActor, OutputFormat="mp3", Text=text
     )
@@ -22,6 +23,32 @@ def getSpeech(text, voiceActor):
     file.write(response["AudioStream"].read())
     file.close()
 
+def download_file(url, file_path):
+        reply = requests.get(url, stream=True, verify = False)
+        with open(file_path, 'wb') as file:
+            for chunk in reply.iter_content(chunk_size=1024): 
+                if chunk:
+                    file.write(chunk)
+
+def transcribe(s3URL, lang):
+    transcribe = boto3.client('transcribe', region_name="us-west-2")
+    job_name = "translateTests"
+    job_uri = s3URL
+    transcribe.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': job_uri},
+        MediaFormat='mp3',
+        LanguageCode= lang
+    )
+    while True:
+        status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        print("Not ready yet...")
+        time.sleep(5)
+    urlPath = status.get('TranscriptionJob').get('Transcript').get('TranscriptFileUri')
+    download_file(urlPath, "/Users/ahotti/Desktop/aws/userFiles/speech.txt")
+    transcribe.delete_transcription_job(TranscriptionJobName=job_name)
 
 def getTranslation(text, countryCode):
     translate = boto3.client(
--- test.py
+++ test.py
@@ -7,14 +7,19 @@ import requests
 transcribe = boto3.client('transcribe')
 
 job_name = "translateTests"
-job_uri = "https://project1-cmpe172.s3-us-west-2.amazonaws.com/speech.mp3"
+job_uri = "https://project1-cmpe172.s3-us-west-2.amazonaws.com/trevor_trump.mp3"
 
 transcribe.start_transcription_job(
     TranscriptionJobName=job_name,
     Media={'MediaFileUri': job_uri},
     MediaFormat='mp3',
-    LanguageCode='en-US'
-
+    LanguageCode='en-US',
+    Settings = 
+    {
+        "ShowSpeakerLabels": True,
+        "MaxSpeakerLabels": 2,
+    }
+    
 )
 
 def download_file(url, file_path):
@@ -31,5 +36,5 @@ while True:
     print("Not ready yet...")
     time.sleep(5)
 urlPath = status.get('TranscriptionJob').get('Transcript').get('TranscriptFileUri')
-download_file(urlPath, "/Users/ahotti/Desktop/aws/userFiles/speech.txt")
+download_file(urlPath, "/Users/ahotti/Desktop/aws/userFiles/speech.json")
 transcribe.delete_transcription_job(TranscriptionJobName=job_name)
